21c726b21c5ff149420c984485adc6101830e8ec
isabele <isabelelima614@gmail.com>
2025-05-13 22:41:25 -0300

    Criado adaptee do tipo Samsung

---
+++ Padroes_de_projeto/Adapter/ProjetorSamsung.java
@@ -0,0 +1,8 @@
+package Padroes_de_projeto.Adapter;
+
+// Adaptee com sua própria interface (turnOn)
+class ProjetorSamsung {
+    public void turnOn() { 
+      System.out.println("Ligando projetor da Samsung");
+    }
+}
